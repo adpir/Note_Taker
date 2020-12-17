7d988bb8e7e4765115f173eb648a7da9149e4a00
Alice Piar <adpir30@gmail.com>
2020-12-16 23:16:33 -0400

    push note, updating error

--- routes/api.js
+++ routes/api.js
@@ -22,21 +22,21 @@ app.post("/api/notes", function(req, res) {
     for(let i=0; i < newNote.length; i++) {
         newNote[i].id=i;
     }
-        // id:Math.floor(Math.random()*100),
-        // title: req.body.title,
-        // text:req.body.text,
+       Math.floor(Math.random()*100),
+       req.body.title,
+       req.body.text,
     newNote=JSON.stringify(newNote);
     fs.writeFileSync("./db/db.json",newNote);
     res.json(JSON.parse(newNote));
     
 
-    // console.log("post",newNote)
-    // db.push(newNote);
-    // fs.writeFileSync("./db/db.json", (db),function (error){
-    //     if (error) throw error
-    //     console.log(db);
-    //     res.json(db);
-    // });
+    console.log("post",newNote)
+     db.push(newNote);
+    fs.writeFileSync("./db/db.json", (db),function (error){
+     if (error) throw error
+       console.log(db);
+       res.json(db);
+   });
 })
 
 app.delete("/api/notes/id", function(req, res) {
